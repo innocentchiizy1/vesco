6683999d42088efd7f7361e3f586c454b7967e69
innocentchiizy1 <innocentchiizy1@gmail.com>
2018-09-07 23:31:46 +0100

    updated

--- js/script.js
+++ js/script.js
@@ -34,3 +34,40 @@ $(function () {
         autoplayHoverPause: true,
     });
 });
+/*=============STATUS============*/
+$(function () {
+    $('.counter').counterUp({
+        delay: 10,
+        time: 3000,
+    });
+});
+/*=============CLIENTS============*/
+$(function () {
+    $("#clients-list").owlCarousel({
+        items: 6,
+        autoplay: true,
+        smartSpeed: 700,
+        loop: true,
+        autoplayHoverPause: true,
+    });
+});
+/*=============NAVIGATION============*/
+$(function () {
+    $(window).scroll(function () {
+        if ($(this).scrollTop() < 50) {
+            $("nav").removeClass("vesco-top-nav");
+        } else {
+            $("nav").addClass("vesco-top-nav");
+        }
+    });
+});
+/*=============SMOOTH SCROLLING============*/
+$(function () {
+    $("a.smooth-scroll").click(function (event) {
+        event.preventDefault();
+        var section = $(this).attr("href");
+        $("html, body").animate({
+            scrollTop: $(section).offset().top - 64
+        }, 1250, "easeInOutExpo");
+    });
+});

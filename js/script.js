18a3422269f8599b8eb2ca37aefa5cfe8458e827
innocentchiizy1 <innocentchiizy1@gmail.com>
2018-09-07 15:21:44 +0100

    update

--- js/script.js
+++ js/script.js
@@ -2,3 +2,35 @@
 $(function () {
     new WOW().init();
 });
+
+
+/*=============WORKS============*/
+$(function () {
+    $("#work").magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        gallery: {
+            enabled: true
+        }
+    });
+});
+/*=============TEAMS============*/
+$(function () {
+    $("#team-members").owlCarousel({
+        items: 3,
+        autoplay: true,
+        smartSpeed: 700,
+        loop: true,
+        autoplayHoverPause: true,
+    });
+});
+/*=============TESTIMONIALS============*/
+$(function () {
+    $("#customers-testimonials").owlCarousel({
+        items: 1,
+        autoplay: true,
+        smartSpeed: 700,
+        loop: true,
+        autoplayHoverPause: true,
+    });
+});
